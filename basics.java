e79c386994c33a5460ac3a87e83a55c0fb5759e3
Daksh Joshi <dakshjoshi1355@gmail.com>
2026-07-14 22:24:55 +0530

    basics

---
+++ basics.java
@@ -0,0 +1,17 @@
+public class basics {
+    
+    public static void main(String[] args) {
+        byte num = 100;
+        System.out.println(num);
+
+        int num1 = 21;
+        int num2 = num1++;
+        
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+
+        String name = "daksh joshi";
+        System.out.println(name.substring(0,5));
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,23 @@
+public class variables{
+    public static void main(String[] args) {
+    
+            int age = 98;
+            System.out.println(age);
+
+            double quantity = 45.55 ;
+            System.out.println(quantity);
+
+            char grade = 'A';
+            System.out.println(grade);
+
+            boolean isStudent = false;
+            System.out.println(isStudent);
+
+            if(isStudent) {
+                System.out.println("You are student");
+            } else System.out.println("You are student!!!!");
+                
+
+
+    }
+}
